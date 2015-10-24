9c901614fd032e42e4219bfa46c93fdd116ceb4e
Carl <nemesisesq@gmail.com>
2015-10-24 16:55:09 -0400

    test

--- scripts/get_shows_by_provider.py
+++ scripts/get_shows_by_provider.py
@@ -1,7 +1,10 @@
 __author__ = 'Nem'
+import django
 
 from server.populate_data.guidebox import *
 from server.populate_data.netflixable import *
+
+django.setup()
 
 def run():
     # get shows by source
