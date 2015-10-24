284f2bebff862a489d1eefca76e68e5f32db5292
Carl <nemesisesq@gmail.com>
2015-10-24 00:30:11 -0400

    set up redis queue for workers

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn cutthecord.wsgi --log-file -
+worker: python worker.py
--- scripts/testfile.py
+++ scripts/testfile.py
@@ -1,8 +1,16 @@
 import sys
 from server.models import *
 
+import time
+
 def run(*args, **kwargs):
     print(args[0])
     sys.stdout.write(args[1])
     sys.stdout.flush()
+
+
+def count():
+    for i in range(100):
+        time.sleep(1)
+        print(i)
 
