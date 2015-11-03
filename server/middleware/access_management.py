8c6283dc81cbe584ff0dee5980ebbd290e8d77d7
Carl <nemesisesq@gmail.com>
2015-11-02 22:22:20 -0500

    middleware change

--- server/middleware/access_management.py
+++ server/middleware/access_management.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from django.shortcuts import redirect
 
@@ -6,10 +7,13 @@ __author__ = 'Nem'
 
 class BlockLiveSiteFromNonAdminUsers(object):
 
+    logger = logging.getLogger('cutthecord')
+
     def process_request(self, request):
 
         # os.environ['ENVIRONMENT']= 'production'
         re = request
+        self.logger.debug('Checking environment {} for user {}'.format(os.getenv('Environment'), re.user))
         if os.getenv('ENVIRONMENT') == 'production':
             if request.user.is_staff or request.path_info.startswith('/admin/'):
                 return None
