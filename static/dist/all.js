5966dfc2b72327c38a907573612eb16dbb64cc3f
Carl <nemesisesq@gmail.com>
2015-11-08 17:05:31 -0500

    step 1 vw

--- static/dist/all.js
+++ static/dist/all.js
@@ -419,11 +419,6 @@ app.service('PackageService', ['http','_', function (http, _) {
 app.factory('_', function($window){
     return $window._;
 })
-
-/**
- * Created by Nem on 10/7/15.
- */
-
 app.controller('chart', function ($scope, http, _, $rootScope) {
     $scope.showArray = [];
     $scope.providers = [];
@@ -523,6 +518,11 @@ app.controller('chart', function ($scope, http, _, $rootScope) {
 
     $rootScope.load()
 });
+
+/**
+ * Created by Nem on 10/7/15.
+ */
+
 /**
  * Created by chirag on 8/3/15.
  */
