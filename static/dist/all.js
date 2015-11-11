f49fd296e362a0efe4793bf8dfbf03735501eaa3
chirag <cmasekar@gmail.com>
2015-11-10 23:33:19 -0500

    much needed refactoring on step four, including width changes

--- static/dist/all.js
+++ static/dist/all.js
@@ -420,6 +420,10 @@ app.factory('_', function($window){
     return $window._;
 })
 
+/**
+ * Created by Nem on 10/7/15.
+ */
+
 app.controller('chart', function ($scope, http, _, $rootScope) {
     $scope.showArray = [];
     $scope.providers = [];
@@ -519,10 +523,6 @@ app.controller('chart', function ($scope, http, _, $rootScope) {
 
     $rootScope.load()
 });
-/**
- * Created by Nem on 10/7/15.
- */
-
 /**
  * Created by chirag on 8/3/15.
  */
