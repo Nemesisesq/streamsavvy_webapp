d715332379fe13f217b7816568608a3363b9fd80
chirag <cmasekar@gmail.com>
2015-11-29 13:44:10 -0500

    hardware and package totals showing up correctly on step three

--- static/dist/all.js
+++ static/dist/all.js
@@ -1278,13 +1278,33 @@ app.controller('StepOneController', function ($scope, $http, $timeout, PackageFa
 app.controller('StepThreeController', function ($scope, PackageFactory) {
 
     $scope.package = PackageFactory.getPackage();
-
+    $scope.hardwareTotal = getHardwareTotal();
+    $scope.servicesTotal = 9.99;
+    $scope.packageTotal = getPackageTotal();
     $scope.$watch(function () {
         return PackageFactory.getPackage()
     }, function () {
         $scope.package = PackageFactory.getPackage();
-    })
-    
+    });
+    function getHardwareTotal() {
+        var hardTotal = 0;
+        for(var i= 0; i<$scope.package.hardware.length;i++)
+        {
+            hardTotal += ($scope.package.hardware[i].retail_cost);
+        }
+        hardTotal = parseFloat(hardTotal.toFixed(2));
+        return hardTotal;
+    }
+
+    function getPackageTotal() {
+        var packTotal = 0;
+        packTotal = $scope.hardwareTotal + $scope.servicesTotal;
+        packTotal = parseFloat(packTotal.toFixed(2));
+
+        return packTotal;
+    }
+
+
 })
 /**
  * Created by Nem on 11/25/15.
