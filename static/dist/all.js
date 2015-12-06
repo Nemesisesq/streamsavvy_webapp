99229b606d919c79520a55e26392f36774afd0b6
chirag <cmasekar@gmail.com>
2015-12-06 15:51:57 -0500

    made changes to both user journey images on homepage, as a result - journey two header image ratio is fixed

--- static/dist/all.js
+++ static/dist/all.js
@@ -687,10 +687,6 @@ app.controller('chart', function ($scope, http, _, $rootScope) {
     $rootScope.load()
 });
 /**
- * Created by Nem on 10/7/15.
- */
-
-/**
  * Created by chirag on 8/3/15.
  */
 app.controller('home', function ($scope, $http, http, $cookies, $location) {
@@ -718,6 +714,10 @@ app.controller('home', function ($scope, $http, http, $cookies, $location) {
 
 
 });
+
+/**
+ * Created by Nem on 10/7/15.
+ */
 
 //app.controller('JourneyOneController', function ($scope, $rootScope, http, _, PackageFactory) {
 //    $scope.hardware = [];
