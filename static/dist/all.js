b382d94d6ab91cb2e55e125c200190a291810c97
Carl <nemesisesq@gmail.com>
2015-12-19 00:19:40 -0500

    minor changes

--- static/dist/all.js
+++ static/dist/all.js
@@ -1342,6 +1342,82 @@ app.controller('search', function ($scope, $http, http, PackageFactory, _, Fuse,
 });
 
 
+
+
+app.controller('ModalController', function ($scope, http, $modal, $log, $rootScope) {
+
+
+    $scope.login = 'Click Here to Login'
+
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $rootScope.openLogInModal = function () {
+        //debugger;
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: '/static/partials/modal/modal.html',
+            controller: 'ModalInstanceController',
+            size: 'sm',
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selectedItem = selectedItem;
+
+
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    }
+
+    if ($rootScope.currentStep == 3) {
+        $rootScope.openLogInModal()
+    }
+});
+
+app.controller('ModalInstanceController', function ($scope, $modalInstance, items, $location, CONFIG) {
+
+    $scope.socialLogin = true;
+
+
+    $scope.facebookAuth = function () {
+
+    window.location = CONFIG.URL + $('#facebook_login').attr('href');
+    }
+
+    $scope.instagramAuth = function () {
+
+    window.location = CONFIG.URL + $('#instagram_login').attr('href');
+    }
+
+    $scope.twitterAuth = function () {
+
+     window.location = CONFIG.URL + $('#twitter_login').attr('href');
+    }
+
+
+
+
+    $scope.items = items;
+
+    $scope.selected = {
+        item: $scope.items[0]
+    }
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+    }
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel')
+    }
+
+})
 /**
  * Created by Nem on 10/27/15.
  */
@@ -1514,82 +1590,6 @@ app.controller('StepOneController', function ($scope, $http, $timeout, PackageFa
 
 });
 
-
-
-app.controller('ModalController', function ($scope, http, $modal, $log, $rootScope) {
-
-
-    $scope.login = 'Click Here to Login'
-
-
-    $scope.items = ['item1', 'item2', 'item3'];
-
-    $rootScope.openLogInModal = function () {
-        //debugger;
-        var modalInstance = $modal.open({
-            animation: true,
-            templateUrl: '/static/partials/modal/modal.html',
-            controller: 'ModalInstanceController',
-            size: 'sm',
-            resolve: {
-                items: function () {
-                    return $scope.items;
-                }
-            }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selectedItem = selectedItem;
-
-
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
-    }
-
-    if ($rootScope.currentStep == 3) {
-        $rootScope.openLogInModal()
-    }
-});
-
-app.controller('ModalInstanceController', function ($scope, $modalInstance, items, $location, CONFIG) {
-
-    $scope.socialLogin = true;
-
-
-    $scope.facebookAuth = function () {
-
-    window.location = CONFIG.URL + $('#facebook_login').attr('href');
-    }
-
-    $scope.instagramAuth = function () {
-
-    window.location = CONFIG.URL + $('#instagram_login').attr('href');
-    }
-
-    $scope.twitterAuth = function () {
-
-     window.location = CONFIG.URL + $('#twitter_login').attr('href');
-    }
-
-
-
-
-    $scope.items = items;
-
-    $scope.selected = {
-        item: $scope.items[0]
-    }
-
-    $scope.ok = function () {
-        $modalInstance.close($scope.selected.item);
-    }
-
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel')
-    }
-
-})
 app.controller('StepThreeController', function ($scope, PackageFactory) {
 
     $scope.package = PackageFactory.getPackage();
