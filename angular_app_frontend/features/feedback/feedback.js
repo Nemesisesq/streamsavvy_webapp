c1997c7f39aaece651e99c36c56901a9b7d53a72
Carl <nemesisesq@gmail.com>
2016-01-03 16:09:05 -0500

    removed feedback form for mobile

--- angular_app_frontend/features/feedback/feedback.js
+++ angular_app_frontend/features/feedback/feedback.js
@@ -3,6 +3,9 @@
  */
 
 app.controller('FeedbackCtrl', function ($scope) {
+    debugger
+    $scope.isMobile = window.innerWidth > 540;
+
     $scope.options = {
         ajaxURL: 'feedback/'
 
--- static/all.js
+++ static/all.js
@@ -962,6 +962,9 @@ app.controller('chart', function ($scope, http, _, $rootScope) {
  */
 
 app.controller('FeedbackCtrl', function ($scope) {
+    debugger
+    $scope.isMobile = window.innerWidth > 540;
+
     $scope.options = {
         ajaxURL: 'feedback/'
 
