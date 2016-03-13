6d9e1298c92a8465daaf7dc5fa947b6f20bedfe7
Chirag Masekar <chirag@Chirags-MacBook-Pro.local>
2016-03-13 11:06:24 -0400

    hardware panel cleanup

--- static/all.js
+++ static/all.js
@@ -1238,101 +1238,6 @@ $(document).ready(function () {
     //
     //}
 });
-app.controller('ProgressController', function ($scope, $state, $rootScope, $location, PackageFactory, $interval) {
-
-    var package = PackageFactory.getPackage();
-
-    //$interval(function(){
-    //     ;
-    //    //package = PackageFactory.getPackage();
-    //    //$scope.package  = package;
-    //}, 500);
-
-    $scope.package = package;
-
-    var stateStep = $state.current.data.step;
-    $scope.stateStep = stateStep;
-    $rootScope.currentStep = stateStep;
-    $scope.step = {
-        one: {
-            text: 'Step One',
-            show: false,
-            active: false
-        },
-        two: {
-            text: 'Step Two',
-            show: false,
-            active: false
-        },
-        three: {
-            text: 'Step Three',
-            show: false,
-            active: false
-        },
-        four: {
-            text: 'Step Four',
-            show: false,
-            active: false
-        }
-    };
-
-    $scope.isActive = function (step) {
-        if (stateStep == step) {
-            return true
-        } else {
-            return false
-        }
-
-
-        return 'inactive'
-    }
-
-    $scope.navigate = function (stateStep) {
-
-        if ($scope.stateStep > stateStep)
-            $location.path('/getting-started/step/' + stateStep)
-
-    }
-
-    if (stateStep == 1) {
-        $scope.step.one.show = true
-
-    } else if (stateStep == 2) {
-        $scope.step.two.show = true
-
-
-    } else if (stateStep == 3) {
-        $scope.step.three.show = true
-
-    } else if (stateStep == 4) {
-        $scope.step.four.show = true
-
-    }
-
-    $scope.progressBar = function (step) {
-        package = PackageFactory.getPackage();
-        var barValue = 0;
-
-        // ;
-
-        if (!_.isEmpty(package) && 2 == $scope.stateStep && 2 == step) {
-
-            barValue = package.hardware.length/3 *100 || 0;
-        }
-
-         ;
-
-        if(!_.isEmpty(package) && 1 == $scope.stateStep && 1 ==step) {
-
-            barValue = package.content.length/5 * 100 || 0;
-        }
-
-
-        return $scope.stateStep > step ? 100 : barValue;
-    }
-});
-
-
 
 /**
  * Created by Nem on 7/18/15.
@@ -1436,6 +1341,101 @@ app.controller('search', function ($scope, $rootScope, $http, http, PackageFacto
 
 })
 ;
+
+
+app.controller('ProgressController', function ($scope, $state, $rootScope, $location, PackageFactory, $interval) {
+
+    var package = PackageFactory.getPackage();
+
+    //$interval(function(){
+    //     ;
+    //    //package = PackageFactory.getPackage();
+    //    //$scope.package  = package;
+    //}, 500);
+
+    $scope.package = package;
+
+    var stateStep = $state.current.data.step;
+    $scope.stateStep = stateStep;
+    $rootScope.currentStep = stateStep;
+    $scope.step = {
+        one: {
+            text: 'Step One',
+            show: false,
+            active: false
+        },
+        two: {
+            text: 'Step Two',
+            show: false,
+            active: false
+        },
+        three: {
+            text: 'Step Three',
+            show: false,
+            active: false
+        },
+        four: {
+            text: 'Step Four',
+            show: false,
+            active: false
+        }
+    };
+
+    $scope.isActive = function (step) {
+        if (stateStep == step) {
+            return true
+        } else {
+            return false
+        }
+
+
+        return 'inactive'
+    }
+
+    $scope.navigate = function (stateStep) {
+
+        if ($scope.stateStep > stateStep)
+            $location.path('/getting-started/step/' + stateStep)
+
+    }
+
+    if (stateStep == 1) {
+        $scope.step.one.show = true
+
+    } else if (stateStep == 2) {
+        $scope.step.two.show = true
+
+
+    } else if (stateStep == 3) {
+        $scope.step.three.show = true
+
+    } else if (stateStep == 4) {
+        $scope.step.four.show = true
+
+    }
+
+    $scope.progressBar = function (step) {
+        package = PackageFactory.getPackage();
+        var barValue = 0;
+
+        // ;
+
+        if (!_.isEmpty(package) && 2 == $scope.stateStep && 2 == step) {
+
+            barValue = package.hardware.length/3 *100 || 0;
+        }
+
+         ;
+
+        if(!_.isEmpty(package) && 1 == $scope.stateStep && 1 ==step) {
+
+            barValue = package.content.length/5 * 100 || 0;
+        }
+
+
+        return $scope.stateStep > step ? 100 : barValue;
+    }
+});
 
 
 app.controller('ServicePanelController', function ($scope, $http, $timeout, PackageFactory, VIEW_WINDOWS) {
@@ -1763,6 +1763,247 @@ app.controller('ShowGridController', function ($scope, $rootScope, $q, $http, $t
         PackageFactory.setPackage($scope.package)
     })
 });
+/**
+ * Created by Nem on 10/27/15.
+ */
+
+app.controller('AccordionController', function ($scope) {
+
+
+    $scope.viewingWindows = [
+        {
+            viewType: "onDemand",
+            description: "I want to watch this show with and <strong> On Demand Subscription. </strong>",
+            parenthetical: "(day/+ after live airing).",
+            dropdown: true,
+        },
+        {
+            viewType: "live",
+            description: "I want to watch this show <strong>Live Over the Air.</strong>",
+            parenthetical: "",
+            dropdown: false,
+        },
+        {
+            viewType: "fullseason",
+            description: " I want to watch this show using a <strong>Full Season Subscription.</strong>",
+            parenthetical: "(season behind).",
+            dropdown: true,
+        },
+        {
+            viewType: "I want to <strong>Pay Per Episode</strong> to watch this show.",
+            description: "",
+            parenthetical: "(day/+ after live airing).",
+            dropdown: false,
+        },
+        //{
+        //    viewType: "",
+        //    description: "",
+        //    parenthetical: "",
+        //    dropdown: false,
+        //}
+    ]
+});
+
+app.controller('StepOneController', function ($scope, $http, $timeout, PackageFactory, VIEW_WINDOWS) {
+
+    $scope.clearContent = function () {
+        var pkg = PackageFactory.getPackage()
+
+        pkg.content = []
+
+        PackageFactory.setPackage(pkg)
+    }
+
+    $scope.showTotal = function (content) {
+
+
+        var total = 0
+
+        var chans = _.map(VIEW_WINDOWS, function (w) {
+
+
+            if (content.viewingWindows !== undefined && content.viewingWindows[w.type] !== undefined) {
+                var window = content.viewingWindows[w.type];
+                if (window.channel !== undefined) {
+
+                    return window.channel;
+
+                }
+            }
+        })
+
+        chans = _.uniq(_.compact(chans), function (c) {
+            if (c.service !== undefined) {
+                return c.service
+            }
+            return c.source
+        })
+        var prices = _.map(chans, function (elem) {
+            return elem.price
+        })
+
+        total = _.reduce(prices, function (total, n) {
+            return total + n;
+        })
+
+
+        //_.forEach($scope.directiveVW, function (window) {
+        //
+        //    if (content.viewingWindows !== undefined && content.viewingWindows[window.type] !== undefined) {
+        //
+        //        var window = content.viewingWindows[window.type];
+        //        if (window.channel !== undefined && window.channel.price !== undefined) {
+        //
+        //            total += window.channel.price;
+        //
+        //        }
+        //
+        //    }
+        //})
+
+        content.totalCost = total
+
+
+        total = _.round(total, 2)
+
+        return total
+
+
+    }
+
+
+    $scope.totalServiceCost = PackageFactory.totalServiceCost;
+
+    //$scope.contentTotal = function () {
+    //
+    //
+    //    var t = 0
+    //
+    //    var package = $scope.package;
+    //    if (package.content.length > 0) {
+    //
+    //         t = _.map(package.providers, function(elem){
+    //            return elem.price;
+    //        })
+    //
+    //        t = _.compact(t);
+    //
+    //        t = _.reduce(t, function(total, n){
+    //            return total + n
+    //        })
+    //    }
+    //
+    //    t = _.round(t, 2)
+    //
+    //    return t
+    //
+    //
+    //}
+
+
+    $scope.directiveVW = [
+
+        {
+            type: 'live',
+            headerText: 'Live Over the Air.',
+            toolTip: 'get your content as soon as it dropped.'
+
+
+        },
+        {
+            type: 'onDemand',
+            headerText: 'On Demand Subscription.',
+            toolTip: 'day/+ after live airing.'
+
+
+        },
+        {
+            type: 'fullseason',
+            headerText: 'Binge Watch Full Seasons',
+            toolTip: 'season behind.'
+
+
+        },
+        {
+            type: 'alacarte',
+            headerText: 'Watch Current Season or Episodes for a fee',
+            toolTip: 'day/+ after live airing with no committment'
+
+
+        },
+
+
+    ]
+
+    $scope.popularShows = null;
+
+    $http.get('api/popular-shows')
+        .success(function (data) {
+            $scope.popularShows = data.results;
+            return data
+        })
+        .then(function () {
+            // ;
+            //$('.popular-shows').slick();
+        });
+
+
+    $scope.package = PackageFactory.getPackage();
+
+    $scope.onDemandLength = function (c) {
+
+        return _.filter(c, function (n) {
+                return n.name == 'Netflix'
+            }).length > 0
+    }
+
+    $scope.delete = function (content) {
+        debugger;
+        _.remove($scope.package.content, content);
+        $scope.savePackage()
+        PackageFactory.updatePackageChannels($scope)
+    }
+
+    $scope.prePopulateWindowProvider = function (content, prop) {
+        var array = _.filter(content.content_provider, function (prov) {
+            return _.includes(_.map($scope.package.providers, function (elem) {
+                return elem.name
+            }), prov.name)
+        })
+
+        if (prop == 'onDemand') {
+
+            _.remove(array, function (n) {
+                return n.name == 'Netflix';
+            })
+        } else if (prop == 'fullSeason') {
+
+            _.remove(array, function (n) {
+                return n.name != 'Netflix';
+            })
+        }
+
+        return _.isEmpty(array) ? false : _.first(array).name;
+
+    }
+
+
+    $scope.$watch(function () {
+        return PackageFactory.getPackage()
+    }, function () {
+        $scope.package = PackageFactory.getPackage();
+    })
+
+
+    $scope.savePackage = function () {
+        PackageFactory.setPackage($scope.package)
+    }
+
+    //$scope.$watchCollection('package.content', function () {
+    //
+    //    PackageFactory.setPackage($scope.package)
+    //})
+});
 app.controller('ModalController', function ($scope, http, $modal, $log, $rootScope) {
 
 
@@ -2016,245 +2257,4 @@ app.controller('StepTwoController', function ($scope, http, PackageFactory) {
     });
 
 
-});
-/**
- * Created by Nem on 10/27/15.
- */
-
-app.controller('AccordionController', function ($scope) {
-
-
-    $scope.viewingWindows = [
-        {
-            viewType: "onDemand",
-            description: "I want to watch this show with and <strong> On Demand Subscription. </strong>",
-            parenthetical: "(day/+ after live airing).",
-            dropdown: true,
-        },
-        {
-            viewType: "live",
-            description: "I want to watch this show <strong>Live Over the Air.</strong>",
-            parenthetical: "",
-            dropdown: false,
-        },
-        {
-            viewType: "fullseason",
-            description: " I want to watch this show using a <strong>Full Season Subscription.</strong>",
-            parenthetical: "(season behind).",
-            dropdown: true,
-        },
-        {
-            viewType: "I want to <strong>Pay Per Episode</strong> to watch this show.",
-            description: "",
-            parenthetical: "(day/+ after live airing).",
-            dropdown: false,
-        },
-        //{
-        //    viewType: "",
-        //    description: "",
-        //    parenthetical: "",
-        //    dropdown: false,
-        //}
-    ]
-});
-
-app.controller('StepOneController', function ($scope, $http, $timeout, PackageFactory, VIEW_WINDOWS) {
-
-    $scope.clearContent = function () {
-        var pkg = PackageFactory.getPackage()
-
-        pkg.content = []
-
-        PackageFactory.setPackage(pkg)
-    }
-
-    $scope.showTotal = function (content) {
-
-
-        var total = 0
-
-        var chans = _.map(VIEW_WINDOWS, function (w) {
-
-
-            if (content.viewingWindows !== undefined && content.viewingWindows[w.type] !== undefined) {
-                var window = content.viewingWindows[w.type];
-                if (window.channel !== undefined) {
-
-                    return window.channel;
-
-                }
-            }
-        })
-
-        chans = _.uniq(_.compact(chans), function (c) {
-            if (c.service !== undefined) {
-                return c.service
-            }
-            return c.source
-        })
-        var prices = _.map(chans, function (elem) {
-            return elem.price
-        })
-
-        total = _.reduce(prices, function (total, n) {
-            return total + n;
-        })
-
-
-        //_.forEach($scope.directiveVW, function (window) {
-        //
-        //    if (content.viewingWindows !== undefined && content.viewingWindows[window.type] !== undefined) {
-        //
-        //        var window = content.viewingWindows[window.type];
-        //        if (window.channel !== undefined && window.channel.price !== undefined) {
-        //
-        //            total += window.channel.price;
-        //
-        //        }
-        //
-        //    }
-        //})
-
-        content.totalCost = total
-
-
-        total = _.round(total, 2)
-
-        return total
-
-
-    }
-
-
-    $scope.totalServiceCost = PackageFactory.totalServiceCost;
-
-    //$scope.contentTotal = function () {
-    //
-    //
-    //    var t = 0
-    //
-    //    var package = $scope.package;
-    //    if (package.content.length > 0) {
-    //
-    //         t = _.map(package.providers, function(elem){
-    //            return elem.price;
-    //        })
-    //
-    //        t = _.compact(t);
-    //
-    //        t = _.reduce(t, function(total, n){
-    //            return total + n
-    //        })
-    //    }
-    //
-    //    t = _.round(t, 2)
-    //
-    //    return t
-    //
-    //
-    //}
-
-
-    $scope.directiveVW = [
-
-        {
-            type: 'live',
-            headerText: 'Live Over the Air.',
-            toolTip: 'get your content as soon as it dropped.'
-
-
-        },
-        {
-            type: 'onDemand',
-            headerText: 'On Demand Subscription.',
-            toolTip: 'day/+ after live airing.'
-
-
-        },
-        {
-            type: 'fullseason',
-            headerText: 'Binge Watch Full Seasons',
-            toolTip: 'season behind.'
-
-
-        },
-        {
-            type: 'alacarte',
-            headerText: 'Watch Current Season or Episodes for a fee',
-            toolTip: 'day/+ after live airing with no committment'
-
-
-        },
-
-
-    ]
-
-    $scope.popularShows = null;
-
-    $http.get('api/popular-shows')
-        .success(function (data) {
-            $scope.popularShows = data.results;
-            return data
-        })
-        .then(function () {
-            // ;
-            //$('.popular-shows').slick();
-        });
-
-
-    $scope.package = PackageFactory.getPackage();
-
-    $scope.onDemandLength = function (c) {
-
-        return _.filter(c, function (n) {
-                return n.name == 'Netflix'
-            }).length > 0
-    }
-
-    $scope.delete = function (content) {
-        debugger;
-        _.remove($scope.package.content, content);
-        $scope.savePackage()
-        PackageFactory.updatePackageChannels($scope)
-    }
-
-    $scope.prePopulateWindowProvider = function (content, prop) {
-        var array = _.filter(content.content_provider, function (prov) {
-            return _.includes(_.map($scope.package.providers, function (elem) {
-                return elem.name
-            }), prov.name)
-        })
-
-        if (prop == 'onDemand') {
-
-            _.remove(array, function (n) {
-                return n.name == 'Netflix';
-            })
-        } else if (prop == 'fullSeason') {
-
-            _.remove(array, function (n) {
-                return n.name != 'Netflix';
-            })
-        }
-
-        return _.isEmpty(array) ? false : _.first(array).name;
-
-    }
-
-
-    $scope.$watch(function () {
-        return PackageFactory.getPackage()
-    }, function () {
-        $scope.package = PackageFactory.getPackage();
-    })
-
-
-    $scope.savePackage = function () {
-        PackageFactory.setPackage($scope.package)
-    }
-
-    //$scope.$watchCollection('package.content', function () {
-    //
-    //    PackageFactory.setPackage($scope.package)
-    //})
 });
