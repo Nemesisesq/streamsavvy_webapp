52647ebdb58f388c0dfa8e917a3ca5b00acad52e
Chirag Masekar <chirag@Chirags-MacBook-Pro.local>
2016-05-08 14:16:24 -0400

    checkout page i have this service already link works

--- angular_app_frontend/features/checkout/checkout.js
+++ angular_app_frontend/features/checkout/checkout.js
@@ -1,7 +1,37 @@
 app.controller('CheckoutController', function ($scope, $http, $timeout, PackageFactory) {
 
-    var ssPackage = PackageFactory.getPackage();
+
+
+    $scope.package = PackageFactory.getPackage();
     $scope.list = PackageFactory.getListOfServices();
+    $scope.list.added = [];
+    
+    $scope.addService = function (service) {
+        
+        $scope.list.added.push(service.chan.source);
+        service.added = true;
+        /*
+        if (service.hasOwnProperty('selected')) {
+            delete service['selected'];
+            //removeShowFromService();
+        }
+
+        var addService = function () {
+            if (!_.includes($scope.list.added, service)) {
+                $scope.list.added.push(service);
+            }
+            $scope.setListOfServices($scope.list);
+        };
+        addService();
+        PackageFactory.setPackage()
+        */
+    }
+
+    $scope.removeService = function(service) {
+        
+    }
+
+
    
 
 });
--- static/all.js
+++ static/all.js
@@ -1289,8 +1289,38 @@ app.factory('ShowDetailAnimate', function ($timeout, $q) {
 
 app.controller('CheckoutController', function ($scope, $http, $timeout, PackageFactory) {
 
-    var ssPackage = PackageFactory.getPackage();
+
+
+    $scope.package = PackageFactory.getPackage();
     $scope.list = PackageFactory.getListOfServices();
+    $scope.list.added = [];
+    
+    $scope.addService = function (service) {
+        
+        $scope.list.added.push(service.chan.source);
+        service.added = true;
+        /*
+        if (service.hasOwnProperty('selected')) {
+            delete service['selected'];
+            //removeShowFromService();
+        }
+
+        var addService = function () {
+            if (!_.includes($scope.list.added, service)) {
+                $scope.list.added.push(service);
+            }
+            $scope.setListOfServices($scope.list);
+        };
+        addService();
+        PackageFactory.setPackage()
+        */
+    }
+
+    $scope.removeService = function(service) {
+        
+    }
+
+
    
 
 });
