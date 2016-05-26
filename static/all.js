3d5cb5fef3de14d2b82883df9faafdbb3fec0ecd
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-05-26 00:50:08 -0400

    fixed service pannel button added some animation

--- static/all.js
+++ static/all.js
@@ -1762,8 +1762,20 @@ app.controller('search', function ($scope, $rootScope, $http, http, PackageFacto
 /**
  * Created by Nem on 5/24/16.
  */
-app.controller('HardwareController', function($scope){
-    $scope.collapseHardware=true;
+app.controller('HardwareController', function ($scope) {
+
+    $('.service-panel').on('scroll', function () {
+        $('.im-ready').fadeOut()
+    })
+
+    $('.service-panel').on('scroll', function () {
+        debugger
+        _.debounce(function () {
+
+            $('.im-ready').fadeIn()
+        }, 500)()
+    })
+    $scope.collapseHardware = true;
     var serviceHeight = $(window).height() - 46;
 
     if ($scope.collapseHardware) {
@@ -1771,26 +1783,25 @@ app.controller('HardwareController', function($scope){
         $('.service-panel.ng-scope').css({'height': serviceHeight + 'px'});
 
     }
-    
+
     $scope.toggleHardwarePanel = function () {
-               debugger;
-
-
+        debugger;
 
 
         if (!$scope.collapseHardware) {
             $('.service-panel.ng-scope').animate({'height': serviceHeight + 'px'});
-            $('.hardware-panel.ng-scope').animate({'height':'46px'});
+            $('.hardware-panel.ng-scope').animate({'height': '46px'});
         } else {
-            $('.hardware-body').animate({height:'40vh'});
-            $('.hardware-panel.ng-scope').animate({'height':'40vh'});
+            $('.hardware-body').animate({height: '40vh'});
+            $('.hardware-panel.ng-scope').animate({'height': '40vh'});
             $('.service-panel.ng-scope').animate({height: '60vh'});
 
         }
         $scope.collapseHardware = !$scope.collapseHardware;
-        
+
     }
 })
+
 app.controller('ServicePanelController', function ($scope, $http, $timeout, PackageFactory, VIEW_WINDOWS) {
 
     $scope.hello = 'world';
