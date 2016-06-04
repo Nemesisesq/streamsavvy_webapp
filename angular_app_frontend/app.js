641988a74f38b9abda04d21b125a73953c3fca65
Chirag Masekar <cmasekar@gmail.com>
2016-06-04 16:26:47 -0400

    added google play and itunes links

--- angular_app_frontend/app.js
+++ angular_app_frontend/app.js
@@ -33,20 +33,26 @@ var app = angular.module('myApp', ["ui.router", "ngCookies", "ui.bootstrap", "ng
                 price: 0.00,
                 description: 'If a subscription service is not for you, these apps allow you to purchase a show at a time.' +
                 ' Or you can purchase an entire season once it has finished airing.',
-                subscriptionLink: 'https://play.google.com/store/movies/category/TV?hl=en'
+                subscriptionLink: 'https://play.google.com/store/movies/category/TV?hl=en',
+                gPlayLink: '',
+                iOSAppStoreLink: ''
             },
             {
                 name: 'netflix', price: 9.99, description: 'Best described as a "binge watch" service. ' +
             'Typically, full seasons are launched all at once and a season behind what is currently showing on TV. Netflix also offers original programming now.' +
             ' This is also released a full season at a time.',
-                subscriptionLink: 'https://www.netflix.com/'
+                subscriptionLink: 'https://www.netflix.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.netflix.mediaclient&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/netflix/id363590051?mt=8&utm_source=Lifehacker+Newsletter&utm_campaign=84a4c2a72c-UA-142218-1&utm_medium=email'
             },
             {
                 name: 'hulu_plus',
                 price: 7.99,
                 description: 'An on-demand service (think DVR) that offers shows from certain networks (ABC, limited NBC, FOX, CW, etc)' +
                 ' a day after they air. Subscription prices depend on whether or not you purchase an ad-free package or not.',
-                subscriptionLink: 'http://www.hulu.com/start'
+                subscriptionLink: 'http://www.hulu.com/start',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.hulu.livingroomplus',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/hulu/id376510438?mt=8'
             },
             {
                 name: 'amazon_prime',
@@ -54,75 +60,99 @@ var app = angular.module('myApp', ["ui.router", "ngCookies", "ui.bootstrap", "ng
                 description: 'An on-demand and binge combo. Some programming is offered in full-season format and some is on-demand after it airs live.' +
                 ' Some shows are free while others are not. Amazon is also building a strong offering of original shows. ' +
                 'It comes free with an annual Prime membership.',
-                subscriptionLink: 'http://www.amazon.com/gp/video/getstarted'
+                subscriptionLink: 'http://www.amazon.com/gp/video/getstarted',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.amazon.mShop.android.shopping',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/amazon-video/id545519333?mt=8&ign-mpt=uo%3D2'
             },
             {
                 name: 'hbo_now',
                 price: 14.99,
                 description: 'Watch HBO shows the moment they air, on-demand, or binge. They also offer all back seasons of episodes.',
-                subscriptionLink: 'https://order.hbonow.com/'
+                subscriptionLink: 'https://order.hbonow.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.hbo.hbonow&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/hbo-now/id971265422?mt=8&ign-mpt=uo%3D2'
             },
             {
                 name: 'sling-tv', price: 20.00,
                 description: 'Live streaming service that makes shows available as they simultaneously air on cable.' +
                 ' The main Sling package gives you a "skinny bundle" of some of the most popular cable channels (ESPN, CNN, HGTV, etc.) ' +
                 'with the option to add extra mini packages on top of your main Sling package.',
-                subscriptionLink: 'https://www.sling.com/'
+                subscriptionLink: 'https://www.sling.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.sling',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/sling-tv-live-and-on-demand/id945077360?mt=8'
             },
             {
                 name: 'Over The Air',
                 price: 0.00,
                 description: 'Watching over the air (OTA) is like watching live television.' +
                 ' There is no monthly cost, but a digital antenna is needed to pull in the signal.',
-                subscriptionLink: 'http://amzn.com/B00X4RA74A'
+                subscriptionLink: 'http://amzn.com/B00X4RA74A',
+                gPlayLink: '',
+                iOSAppStoreLink: ''
             },
             {
                 name: 'showtime',
                 price: 10.99,
                 description: 'Watch Showtime shows the moment they air, on-demand, or binge. They also offer all back seasons of episodes.',
-                subscriptionLink: 'http://www.sho.com/order?source=acq_shoanytime_about'
+                subscriptionLink: 'http://www.sho.com/order?source=acq_shoanytime_about',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.showtime.showtimeanytime',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/showtime-anytime/id484232467?mt=8'
             },
             {
                 name: 'cbs',
                 price: 5.99,
                 description: 'New CBS episodes on demand the day after they air and almost all past seasons of CBS shows for binging.' +
                 ' In select markets, you can stream CBS live.',
-                subscriptionLink: 'https://www.cbs.com/all-access/'
+                subscriptionLink: 'https://www.cbs.com/all-access/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.cbs.ott',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/cbs/id530168168?mt=8'
             },
             {
                 name: 'nbc',
                 price: 0.00,
                 description: 'A DVR-like, on-demand app that offers shows from NBC a day after they air. You do have to watch commercials, but it\'s free.',
-                subscriptionLink: 'http://www.nbc.com/video'
+                subscriptionLink: 'http://www.nbc.com/video',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.nbcuni.nbc&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/nbc-watch-now-stream-full/id442839435?mt=8'
             },
             {
                 name: 'thecw',
                 price: 0.00,
                 description: 'An on-demand and binge combo for classic CW shows and some new original content.',
-                subscriptionLink: 'http://www.cwseed.com/'
+                subscriptionLink: 'http://www.cwseed.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.cw.seed.android&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/the-cw/id491730359?mt=8'
             },
             {
                 name: 'PBS App',
                 price: 0.00,
                 description: 'A streaming App for PBS.',
-                subscriptionLink: 'http://www.pbs.org/video/'
+                subscriptionLink: 'http://www.pbs.org/video/',
+                gPlayLink: '',
+                iOSAppStoreLink: ''
             },
             {
                 name: 'starz',
                 price: 8.99,
                 description: 'Download and watch past episodes and seasons of your favorite Starz shows. Unlike HBO Now and Showtime,' +
                 ' you can\'t watch shows as they air. They do let you download shows to watch at a later time when you may not have access to wifi.',
-                subscriptionLink: 'https://www.starz.com/buy-starz/?TID='
+                subscriptionLink: 'https://www.starz.com/buy-starz/?TID=',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.bydeluxe.d3.android.program.starz&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/starz/id550221096?mt=8'
             },
             {
                 name: 'Seeso',
                 price: 3.99,
                 description: 'NBC\'s binge watching app for classic and hard-to-find comedy as well as original content. No commercials.',
-                subscriptionLink: 'https://www.seeso.com/'
+                subscriptionLink: 'https://www.seeso.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=seeso.com&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/seeso/id1053181816?mt=8'
             },
             {
                 name: 'tubi_tv', price: 0.00, description: 'Free binge watching app for unique and classic content.',
-                subscriptionLink: 'https://tubitv.com/'
+                subscriptionLink: 'https://tubitv.com/',
+                gPlayLink: 'https://play.google.com/store/apps/details?id=com.tubitv&hl=en',
+                iOSAppStoreLink: 'https://itunes.apple.com/us/app/tubi-tv-stream-free-movies/id886445756?mt=8   '
             }
 
         ])
