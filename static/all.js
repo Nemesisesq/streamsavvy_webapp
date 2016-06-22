43351b5fbd0d3631af4a2ad1928086fb8a85a30a
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-22 09:42:35 -0400

    added media queries and classes for service widths

--- static/all.js
+++ static/all.js
@@ -2211,6 +2211,7 @@ app.factory('ShowDetailAnimate', function ($timeout, $q) {
     }
 });
 
+
 app.controller('CheckoutController', function ($scope, $http, $timeout,$filter, PackageFactory, SERVICE_PRICE_LIST) {
 
     $scope.package = PackageFactory.getPackage();
@@ -2316,7 +2317,6 @@ app.controller('CheckoutController', function ($scope, $http, $timeout,$filter, 
 /**
  * Created by chirag on 3/28/16.
  */
-
 
 /**
  * Created by Nem on 12/29/15.
