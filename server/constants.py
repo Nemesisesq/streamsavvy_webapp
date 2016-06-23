f1eec27fe33a8bc0fa702ed1239fc6a606749367
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-22 20:26:30 -0400

    south park in sling

--- server/constants.py
+++ server/constants.py
@@ -1,6 +1,6 @@
 sling_channels = ['ESPN', 'ESPN2', 'AMC', 'Food Network', 'A&E', 'History', 'TNT', 'El Rey', 'HGTV', 'IFC',
                   'Disney Channel', 'Polaris +', 'Maker', 'TBS', 'Travel Channel', 'Adult Swim', 'CNN', 'H2',
-                  'Cartoon Network', 'ABC Family', 'Lifetime', 'Galavision', 'Bloomberg Television', 'Freeform']
+                  'Cartoon Network', "Comedy Central", 'ABC Family', 'Lifetime', 'Galavision', 'Bloomberg Television', 'Freeform']
 # def add_sling(channel):
 
 broadcast_channels = ['NBC', 'FOX', 'ABC', 'CBS', 'The CW', 'CW', 'PBS']
