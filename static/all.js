68220366ae601c3f77f7a451be0e00623211c601
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-28 14:52:50 -0400

    clean up some styles

--- static/all.js
+++ static/all.js
@@ -2343,6 +2343,7 @@ app.factory('ShowDetailAnimate', function ($timeout, $q, $window) {
     }
 });
 
+
 app.controller('CheckoutController', function ($scope, $http, $timeout,$filter, PackageFactory, SERVICE_PRICE_LIST) {
 
     $scope.package = PackageFactory.getPackage();
@@ -2448,7 +2449,6 @@ app.controller('CheckoutController', function ($scope, $http, $timeout,$filter, 
 /**
  * Created by chirag on 3/28/16.
  */
-
 
 /**
  * Created by Nem on 12/29/15.
