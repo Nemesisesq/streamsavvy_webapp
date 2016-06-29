8b79cccb7ae81dfcee851accef26982cc0615a6d
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-06-29 15:37:46 -0400

    removed restriction on filter

--- scripts/get_available_sources.py
+++ scripts/get_available_sources.py
@@ -6,6 +6,7 @@ from server.tasks import add_available_sources_to_shows
 django.setup()
 
 def run():
+    print('hello')
 
     add_available_sources_to_shows()
 
--- server/tasks.py
+++ server/tasks.py
@@ -65,7 +65,8 @@ def add_available_sources_to_shows():
     q_high = django_rq.get_queue('high')
 
     # all_shows = Content.objects.all()
-    all_shows = Content.objects.filter(guidebox_data__sources='null')
+    all_shows = Content.objects.all()
+    print(all_shows)
     for show in [all_shows[i: i + 20]  for i in range(0, len(all_shows), 20)]:
         q_high.enqueue(g.add_additional_channels_for_show, show)
 
