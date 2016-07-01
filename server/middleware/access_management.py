6d9f2fff5ed349f3e5aacfccb96603cbf7c61583
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-07-01 13:32:01 -0400

    fixed request key error for user

--- server/middleware/access_management.py
+++ server/middleware/access_management.py
@@ -13,7 +13,7 @@ class BlockLiveSiteFromNonAdminUsers(object):
 
         # os.environ['ENVIRONMENT']= 'production'
         re = request
-        self.logger.debug('Checking environment {} for user {}'.format(os.getenv('Environment'), re.user))
+        # self.logger.debug('Checking environment {} for user {}'.format(os.getenv('Environment'), re.user))
         if os.getenv('ENVIRONMENT') == 'production' and os.getenv('REQUIRE_AUTH'):
             if request.user.is_staff or request.user.is_authenticated or request.path_info.startswith('/admin/'):
                 return None
