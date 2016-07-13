12bfee1a458459a347491b7f7b2d006fbbb0d97c
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-07-13 14:09:13 -0400

    limited footer reposition to the home page

--- angular_app_frontend/features/footer/footer.js
+++ angular_app_frontend/features/footer/footer.js
@@ -2,23 +2,34 @@
  * Created by Nem on 10/7/15.
  */
 
-app.directive('footer', function () {
+app.directive('footer', function ($state) {
 
     return {
         restrict: 'E',
         templateUrl: 'static/partials/footer.html',
         link: function (scope, elemtnt, attrs) {
 
+            function fixFooter() {
+
+                if ($state.current.name == 'nav.home') {
+                    $('div[ui-view="home"]').height(window.innerHeight)
+                    console.log('footer fixed')
+                }
+            }
+
             var target = document.querySelector("[ui-view]")
 
             var config = {attributes: true, childList: true, characterData: true};
 
             var observer = new MutationObserver(function (mutations) {
-                // debugger;
+                debugger;
+
+
                 fixFooter();
                 mutations.forEach(function (mutation) {
                     console.log(mutation)
                 })
+
             })
 
             observer.observe(target, config)
@@ -30,11 +41,6 @@ app.directive('footer', function () {
 
 
 })
-
-
-function fixFooter() {
-    $('div[ui-view="home"]').height(window.innerHeight)
-}
 
 $(document).ready(function () {
 
--- static/all.js
+++ static/all.js
@@ -2061,53 +2061,6 @@ app.controller('CheckoutController', function ($scope, $http, $timeout, $filter,
 
 
 /**
- * Created by Nem on 10/7/15.
- */
-
-app.directive('footer', function () {
-
-    return {
-        restrict: 'E',
-        templateUrl: 'static/partials/footer.html',
-        link: function (scope, elemtnt, attrs) {
-
-            var target = document.querySelector("[ui-view]")
-
-            var config = {attributes: true, childList: true, characterData: true};
-
-            var observer = new MutationObserver(function (mutations) {
-                // debugger;
-                fixFooter();
-                mutations.forEach(function (mutation) {
-                    console.log(mutation)
-                })
-            })
-
-            observer.observe(target, config)
-            $(window).resize(fixFooter)
-
-
-        }
-    }
-
-
-})
-
-
-function fixFooter() {
-    $('div[ui-view="home"]').height(window.innerHeight)
-}
-
-$(document).ready(function () {
-
-
-
-
-    // debugger;
-
-})
-
-/**
  * Created by Nem on 12/29/15.
  */
 
@@ -2121,6 +2074,59 @@ app.controller('FeedbackCtrl', function ($scope) {
 
     }
 })
+/**
+ * Created by Nem on 10/7/15.
+ */
+
+app.directive('footer', function ($state) {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'static/partials/footer.html',
+        link: function (scope, elemtnt, attrs) {
+
+            function fixFooter() {
+
+                if ($state.current.name == 'nav.home') {
+                    $('div[ui-view="home"]').height(window.innerHeight)
+                    console.log('footer fixed')
+                }
+            }
+
+            var target = document.querySelector("[ui-view]")
+
+            var config = {attributes: true, childList: true, characterData: true};
+
+            var observer = new MutationObserver(function (mutations) {
+                debugger;
+
+
+                fixFooter();
+                mutations.forEach(function (mutation) {
+                    console.log(mutation)
+                })
+
+            })
+
+            observer.observe(target, config)
+            $(window).resize(fixFooter)
+
+
+        }
+    }
+
+
+})
+
+$(document).ready(function () {
+
+
+
+
+    // debugger;
+
+})
+
 /**
  * Created by chirag on 8/3/15.
  */
