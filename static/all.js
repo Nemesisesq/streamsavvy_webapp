3516fd18d696713be1016641adfdc9f7ae438469
Carl Lewis <Nem@Carls-MacBook-Pro.local>
2016-08-25 12:02:38 -0400

    footer fixes

--- static/all.js
+++ static/all.js
@@ -2170,6 +2170,20 @@ app.controller('CheckoutController', function ($scope, $state, $http, $timeout, 
 
 
 /**
+ * Created by Nem on 12/29/15.
+ */
+
+app.controller('FeedbackCtrl', function ($scope) {
+   
+    $scope.isMobile = window.innerWidth > 540;
+
+    $scope.options = {
+        ajaxURL: 'feedback/',
+        html2canvasURL: 'static/html2Canvas/build/html2canvas.js',
+
+    }
+})
+/**
  * Created by Nem on 10/7/15.
  */
 
@@ -2219,20 +2233,6 @@ $(document).ready(function () {
 
 })
 
-/**
- * Created by Nem on 12/29/15.
- */
-
-app.controller('FeedbackCtrl', function ($scope) {
-   
-    $scope.isMobile = window.innerWidth > 540;
-
-    $scope.options = {
-        ajaxURL: 'feedback/',
-        html2canvasURL: 'static/html2Canvas/build/html2canvas.js',
-
-    }
-})
 /**
  * Created by chirag on 8/3/15.
  */
@@ -3157,120 +3157,6 @@ app.directive('comingSoon', function (_) {
 
 
 /**
- * Created by Nem on 5/24/16.
- */
-app.controller('HardwareController', function ($scope, PackageFactory, $state, $window, loginEventService, $rootScope) {
-
-    $scope.devices = [
-        {
-            name: 'Roku',
-            image: 'https://s3.amazonaws.com/streamsavvy/Roku4.png',
-            url: 'http://www.dpbolvw.net/click-7926773-12551146-1459950711000',
-            price: 49.99
-        },
-        {
-            name: 'Mohu Leaf',
-            image: 'https://s3.amazonaws.com/streamsavvy/Mohu.png',
-            url: 'https://www.amazon.com/Mohu-Paper-thin-Reversible-Performance-MH-110583/dp/B004QK7HI8',
-            price: 39.00
-        }
-    ]
-
-    $scope.state = $state.current.name;
-
-    $scope.linkToAffiliate = function (device) {
-        $window.open(device.url);
-        mixpanel.track("Buy Device", {
-            "id": 11,
-            "service name": device.chan.display_name,
-            "user": $window.sessionStorage.user
-        });
-    }
-
-    $scope.pkg = PackageFactory.getPackage();
-
-    $scope.proceedToCheckout = function () {
-        location.href = '#/checkout';
-
-        $scope.pkg = PackageFactory.getPackage();
-        var showList = _.map($scope.pkg.data.content, function (showObject) {
-            return showObject.title;
-        });
-
-        mixpanel.track("Proceeded to Checkout", {
-            "id": 7,
-            "show_List": showList,
-            "user": $window.sessionStorage.user
-        });
-    };
-
-    if($window.sessionStorage.redirectToCheckout=='true'){
-        $state.go('check.out')
-        $window.sessionStorage.redirectToCheckout = false
-    }
-
-    $scope.go = function () {
-
-        if (!$rootScope.logged_in) {
-            $window.sessionStorage.redirectToCheckout = true
-            loginEventService.broadcast()
-        } else {
-            $scope.proceedToCheckout();
-        }
-
-        $rootScope.$on('login.modal.closed', function () {
-            if (!$window.sessionStorage.token) {
-                mixpanel.track("To Checkout w/o login", {
-                    "id": 17,
-                    // "show_List": showList,
-                    "user": $window.sessionStorage.user
-                })
-
-                $scope.proceedToCheckout()
-            }
-        })
-
-
-    }
-
-    $scope.collapseHardware = true;
-    var serviceHeight = $(window).height() - 46;
-
-    if ($scope.collapseHardware) {
-        $('.service-panel.ng-scope').css({'height': serviceHeight + 'px'});
-
-    }
-
-    $scope.mixpanelTrackReadyToCutCord = function () {
-        var showList = [];
-        _.forEach($scope.pkg.data.content, function (showObject) {
-            showList.push(showObject.title);
-        });
-        // console.log(showList);
-
-    }
-
-    $scope.servicesGT0 = function () {
-        return !_.isEmpty(PackageFactory.getListOfServices())
-    }
-
-    $scope.$watchCollection(function () {
-
-
-            if (PackageFactory.getPackage().data) {
-                return PackageFactory.getPackage().data.content
-            }
-        },
-        function () {
-            if (PackageFactory.getPackage().data) {
-
-                $scope.pkgHasContent = PackageFactory.getPackage().data.content.length > 0
-            }
-        })
-
-});
-
-/**
  * Created by Nem on 3/7/16.
  */
 
@@ -3482,6 +3368,120 @@ app.directive('sportsOverlay', function ($http) {
         }
     }
 })
+
+/**
+ * Created by Nem on 5/24/16.
+ */
+app.controller('HardwareController', function ($scope, PackageFactory, $state, $window, loginEventService, $rootScope) {
+
+    $scope.devices = [
+        {
+            name: 'Roku',
+            image: 'https://s3.amazonaws.com/streamsavvy/Roku4.png',
+            url: 'http://www.dpbolvw.net/click-7926773-12551146-1459950711000',
+            price: 49.99
+        },
+        {
+            name: 'Mohu Leaf',
+            image: 'https://s3.amazonaws.com/streamsavvy/Mohu.png',
+            url: 'https://www.amazon.com/Mohu-Paper-thin-Reversible-Performance-MH-110583/dp/B004QK7HI8',
+            price: 39.00
+        }
+    ]
+
+    $scope.state = $state.current.name;
+
+    $scope.linkToAffiliate = function (device) {
+        $window.open(device.url);
+        mixpanel.track("Buy Device", {
+            "id": 11,
+            "service name": device.chan.display_name,
+            "user": $window.sessionStorage.user
+        });
+    }
+
+    $scope.pkg = PackageFactory.getPackage();
+
+    $scope.proceedToCheckout = function () {
+        location.href = '#/checkout';
+
+        $scope.pkg = PackageFactory.getPackage();
+        var showList = _.map($scope.pkg.data.content, function (showObject) {
+            return showObject.title;
+        });
+
+        mixpanel.track("Proceeded to Checkout", {
+            "id": 7,
+            "show_List": showList,
+            "user": $window.sessionStorage.user
+        });
+    };
+
+    if($window.sessionStorage.redirectToCheckout=='true'){
+        $state.go('check.out')
+        $window.sessionStorage.redirectToCheckout = false
+    }
+
+    $scope.go = function () {
+
+        if (!$rootScope.logged_in) {
+            $window.sessionStorage.redirectToCheckout = true
+            loginEventService.broadcast()
+        } else {
+            $scope.proceedToCheckout();
+        }
+
+        $rootScope.$on('login.modal.closed', function () {
+            if (!$window.sessionStorage.token) {
+                mixpanel.track("To Checkout w/o login", {
+                    "id": 17,
+                    // "show_List": showList,
+                    "user": $window.sessionStorage.user
+                })
+
+                $scope.proceedToCheckout()
+            }
+        })
+
+
+    }
+
+    $scope.collapseHardware = true;
+    var serviceHeight = $(window).height() - 46;
+
+    if ($scope.collapseHardware) {
+        $('.service-panel.ng-scope').css({'height': serviceHeight + 'px'});
+
+    }
+
+    $scope.mixpanelTrackReadyToCutCord = function () {
+        var showList = [];
+        _.forEach($scope.pkg.data.content, function (showObject) {
+            showList.push(showObject.title);
+        });
+        // console.log(showList);
+
+    }
+
+    $scope.servicesGT0 = function () {
+        return !_.isEmpty(PackageFactory.getListOfServices())
+    }
+
+    $scope.$watchCollection(function () {
+
+
+            if (PackageFactory.getPackage().data) {
+                return PackageFactory.getPackage().data.content
+            }
+        },
+        function () {
+            if (PackageFactory.getPackage().data) {
+
+                $scope.pkgHasContent = PackageFactory.getPackage().data.content.length > 0
+            }
+        })
+
+});
 
 app.controller('ServicePanelController', function ($scope, $http, $timeout, PackageFactory) {
 
